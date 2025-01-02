ad6a8d60b3535802fbf98e1aac0375e5914bb7d3
Prince Kumar <princesd991@gmail.com>
2025-01-02 15:13:39 +0530

    🍺 prisma migrate and check user

--- components/header.jsx
+++ components/header.jsx
@@ -2,8 +2,11 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { LayoutDashboard, PenBox } from "lucide-react";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
+
   return (
     <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
